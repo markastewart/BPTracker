a202a023457dac564ab7c18fe4da9c0bbec932a3
yen4life <mark@yenforlife.com>
2024-04-03 22:48:16 -0400

    First attempt at PDF again

--- BPTracker/ContentView.swift
+++ BPTracker/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import UIKit
 
 public extension View {
     @MainActor
@@ -251,7 +252,8 @@ struct ShowReport: View {
         
         HStack {
             Button {
-                printDocumentView(buildPrintView())
+                //printDocumentView(buildPrintView())
+                ShareLink("Export PDF", item: render())
             } label: {
                 Text("Print")
                 Image(systemName: "printer")
@@ -281,6 +283,39 @@ struct ShowReport: View {
         }
         return printableView
     }
+    
+    
+    @MainActor func render() -> URL {
+            // 1: Render Hello World with some modifiers
+        let renderer = ImageRenderer(content:
+                                        buildPrintView())
+        
+            // 2: Save it to our documents directory
+        let url = URL.documentsDirectory.appending(path: "output.pdf")
+        
+            // 3: Start the rendering process
+        renderer.render { size, context in
+                // 4: Tell SwiftUI our PDF should be the same size as the views we're rendering
+            var box = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            
+                // 5: Create the CGContext for our PDF pages
+            guard let pdf = CGContext(url as CFURL, mediaBox: &box, nil) else {
+                return
+            }
+            
+                // 6: Start a new PDF page
+            pdf.beginPDFPage(nil)
+            
+                // 7: Render the SwiftUI view data onto the page
+            context(pdf)
+            
+                // 8: End the page and close the file
+            pdf.endPDFPage()
+            pdf.closePDF()
+        }
+        return url
+    }
+
     
     @MainActor
     func printDocumentView(_ printableView: some View) {
