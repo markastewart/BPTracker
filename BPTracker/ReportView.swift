c85406e009bd15d23370854a13279caadcaa564f
yen4life <mark@yenforlife.com>
2024-04-05 17:43:05 -0400

    Move ShowResults view to ReportView and delete ResultsView

--- BPTracker/ReportView.swift
+++ BPTracker/ReportView.swift
@@ -26,6 +26,57 @@ public extension View {
     }
 }
 
+struct ShowResults: View {
+    @Query(sort: \BPDetails.timestamp) var bpDetailResults: [BPDetails]
+    @Binding var showResults: Bool
+    @State var startDate = Date()
+    @State var endDate = Date()
+    @State var showReport = false
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("Select Blood Pressure Results").font(.headline).padding(.vertical, 35)
+                .onAppear() {
+                    if let firstBPRec = bpDetailResults.first {
+                        startDate = firstBPRec.timestamp
+                    }
+                    if let lastBPRec = bpDetailResults.last {
+                        endDate = lastBPRec.timestamp
+                    }
+                }
+            Spacer()
+        }
+        
+        VStack {
+            HStack {
+                DatePicker("Start Date", selection: $startDate, displayedComponents:.date)
+                Spacer()
+            }.padding(.horizontal, 20)
+            
+            HStack {
+                DatePicker("End Date", selection: $endDate, displayedComponents:.date)
+                Spacer()
+            }.padding(.horizontal, 20)
+            
+            HStack {
+                Button("Show Data") {
+                    showReport = true
+                }
+                .fullScreenCover(isPresented: $showReport, content: { ShowReport(showReport: $showReport, startDate: $startDate, endDate: $endDate) })
+                
+                Spacer()
+                
+                Button("Cancel") {
+                    showResults = false
+                }
+            }.padding(.horizontal, 50).padding(.vertical,20)
+        }
+        .font(.subheadline)
+    }
+}
+
+
 struct ShowReport: View {
     @Query(sort: \BPDetails.timestamp) var bpDetailResults: [BPDetails]
     @Query(sort: \BPDetails.totalmmHg) var mmHgSorted: [BPDetails]
